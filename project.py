8f114bb7478ea6f9d3f9c61179b2b518682e1aa7
Abdul Rehman Khams <111648325+ark331@users.noreply.github.com>
2024-06-16 22:21:19 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,2 +1,10 @@
-
+# Stone paper scissors game
 print("Hello main.py") 
+def choice(p1, p2) :
+  if p1 == 'Rock' and p2 == 'Rock' or p1=='Scissors' and p2=='Scissors' or p1=='Paper' and p2=='Paper':
+    return "Match Drawn"
+  elif p1=='Rock' and p2=='Scissors' or p1=='Scissors' and p2=='Paper' or p1=='Paper' and p2=='Rock' :
+    return " Player 1 Won the game!!! "
+  else :
+    return "Player 2 won the game!!! "
+
