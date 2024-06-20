03c62b05c2d468d777752185b2a5fe22822d794a
Abdul Rehman Khams <111648325+ark331@users.noreply.github.com>
2024-06-20 20:37:18 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 # Stone paper scissors game
 print("Hello main.py") 
+import random 
+
 def choice(p1, p2) :
+  list = ['Rock', 'Paper', 'Scissors']
   if p1 == 'Rock' and p2 == 'Rock' or p1=='Scissors' and p2=='Scissors' or p1=='Paper' and p2=='Paper':
     return "Match Drawn"
   elif p1=='Rock' and p2=='Scissors' or p1=='Scissors' and p2=='Paper' or p1=='Paper' and p2=='Rock' :
@@ -11,5 +14,6 @@ def choice(p1, p2) :
 player1 = input("Enter your Choice (Rock, Paper, Scissor)") 
 print("Computer selecting moves") 
 print("Calculating probabilities ") 
+player2 = random.choice(list) 
                 
 check() 
